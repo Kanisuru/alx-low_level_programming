8829efcac37f5d4e82b530c28529b647de0d3f19
Kanisuru <kanisurudaniel@gmail.com>
2022-09-08 21:08:16 -0700

    hello, put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - the program starts and ends with the main* Return: the main function returns zero when
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
